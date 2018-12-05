523359061d3652be8a8bb0e70c6277044f81168c
SandroCourses <sandroneumann@me.com>
2018-12-05 17:39:51 +0100

    Update form css

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -7,9 +7,9 @@ class InputField extends Component {
 
   render() {
     return (
-      <div className="form-group">
+      <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}className="form-group">
         <label htmlFor={this.props.identification}>{this.props.label}</label>
-        <input type="text" className="form-control" id={this.props.identification} placeholder={this.props.placeholder} onKeyUp={this.handleOnKeyUp} />
+        <input style={{width: '300px'}} type="text" className="form-control" id={this.props.identification} placeholder={this.props.placeholder} onKeyUp={this.handleOnKeyUp} />
       </div>
     );
   }
